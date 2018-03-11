8da09aca3bb0332e9d49caafb00eb507e8e8e070
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-03-11 22:38:14 +0700

    Exercise14

---
+++ Exercise14.js
@@ -0,0 +1,23 @@
+function mengelompokkanAngka(arr) {
+    var genap = [];
+     var ganjil = [];
+     var bagiTiga = [];
+     for(x=0;x<arr.length;x++){
+         if(arr[x]%2 === 0 && arr[x]%3 !== 0){
+             genap.push(arr[x]);
+       }
+         if(arr[x]%2 == 1 && arr[x]%3 !== 0){
+             ganjil.push(arr[x]);
+       }
+         if(arr[x]%3 === 0){
+             bagiTiga.push(arr[x]);
+       }
+     }
+   return([genap,ganjil,bagiTiga]);
+   }
+   
+   // TEST CASES
+   console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+   console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+   console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+   console.log(mengelompokkanAngka([])); // [ [], [], [] ]
