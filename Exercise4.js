bb29fdcc8e0899af384762e169270718e21fdc9e
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-03-11 10:54:15 +0700

    Exercise4

--- Exercise4.js
+++ Exercise4.js
@@ -1,45 +1,52 @@
 // point 1
-var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"]
-console.log (input);
 
+var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"]
+/* function dataHandling2 (input) {
+  input.splice (2, 1,"Provinsi Bandar Lampung");
+["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
+
+}
+//function write here
+*/
 // point 2
-input.splice(2, 0, "Provinsi ");
-input.splice(5, 1);
-input.splice(6, 0, "Pria");
-input.splice(7, 0, "SMA Internasional Metro");
+input.splice(2, 1, "Provinsi Bandar Lampung");
+input.splice(4, 1);
+input.splice(5, 0, "Pria");
+input.splice(6, 0, "SMA Internasional Metro");
 console.log (input);
 
 // point pisah tgl bln
-var tanggal = input[4].split("/");
+var tanggal = input[3].split("/");
 console.log(tanggal);
 
 // point angka kebaca bulan
 var lahirBulan = tanggal[1];
-var bulan = '';
+var bulan = tanggal[1];
+// console.log(bulan);
 switch(lahirBulan) {
-  case 01: bulan = 'January';
+  case '01': bulan = 'January';
     break;
-  case 02: bulan = 'February';
+  case '02': bulan = 'February';
     break;
-   case 03: bulan = 'March';
+   case '03': bulan = 'March';
      break;
-  case 04: bulan = 'April';
+  case '04': bulan = 'April';
     break;
-    case 05: bulan = 'May';
+    case '05': bulan = 'May';
       break;
-  case 06: bulan = 'June';
+  case '06': bulan = 'June';
     break;
-    case 07: bulan = 'July';
+    case '07': bulan = 'July';
       break;
-  case 08: bulan = 'August';
+  case '08': bulan = 'August';
     break;
-    case 09: bulan = 'September';
+    case '09': bulan = 'September';
       break;
-  case 10: bulan = 'October';
+  case '10': bulan = 'October';
     break;
-    case 11: bulan = 'November';
+    case '11': bulan = 'November';
       break;
-  case 12: bulan = 'Desember';
+  case '12': bulan = 'Desember';
     break;
   default:  { console.log('Salah Input Bulan'); }
 }
@@ -47,7 +54,7 @@ console.log (bulan);
 
 // point decending
 var tanggalDecending=tanggal;
-tanggalDecending.sort(function(value0, value1, value2) { return value0, value1 < value2});
+tanggalDecending.sort(function(a, b) { return b - a});
 console.log(tanggalDecending);
 
 // point join
@@ -56,6 +63,6 @@ console.log(tanggal.join('-'));
 
 // to string and slice
 var input;
-console.log(input.toString());
+input.toString();
 var inputTerbaru = input[1].slice(0,15);
 console.log(inputTerbaru);
