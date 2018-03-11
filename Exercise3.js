cb9a3f9b6c57977aa9d6d43f6d2bb212c9b33d2e
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-03-11 10:52:11 +0700

    Exercise3

--- Exercise3.js
+++ Exercise3.js
@@ -3,37 +3,24 @@ var input = [
     ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
     ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
     ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
-]
-console.log ("Nama Id: " + (input[0][0]));
-console.log ("Nama Lengkap: " + (input[0][1]));
-console.log ("TTL: " + (input[0][2] + " "+ (input[0][3])));
-console.log ("Hobby: " + (input[0][4]));
-console.log ("");
-console.log ("");
-console.log ("Nama Id: " + (input[1][0]));
-console.log ("Nama Lengkap: " + (input[1][1]));
-console.log ("TTL: " + (input[1][2] + " "+ (input[1][3])));
-console.log ("Hobby: " + (input[1][4]));
-console.log ("");
-console.log ("");
-console.log ("Nama Id: " + (input[2][0]));
-console.log ("Nama Lengkap: " + (input[2][1]));
-console.log ("TTL: " + (input[2][2] + " "+ (input[2][3])));
-console.log ("Hobby: " + (input[2][4]));
-console.log ("");
-console.log ("");
-console.log ("Nama Id: " + (input[3][0]));
-console.log ("Nama Lengkap: " + (input[3][1]));
-console.log ("TTL: " + (input[3][2] + " "+ (input[3][3])));
-console.log ("Hobby: " + (input[3][4]));
-
-/*
-
-bisa juga pakai ini untuk yang TTL, tapi hasilnya ada komanya.
-
-input[0].pop();
-input[0].shift(0);
-input[0].shift(0);
-console.log ("TTL: " + (input[0]));
-
-*/
+];
+function dataHandling(){
+  for (var i = 0; i < input.length; i++){
+    for (var j = 0; j < input[i].length; j++ ){
+      if (j===0) {
+        console.log('Nomer ID: ' + input[i][j]);
+      }
+      else if (j===1){
+        console.log('Nama Lengkap: ' + input[i][j]);
+      }
+      else if (j===2){
+        console.log('TTL: ' + input[i][j] +' '+ input[i][j+1]);
+      }
+      else if (j===3){
+        console.log('Hobi: ' + input[i][j]);
+      }
+    }
+    console.log();
+  }
+}
+dataHandling(input);
