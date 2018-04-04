1ebd85668a3c65724247c3abc14ef706f4a3a04a
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-04-04 17:02:28 +0700

    Target Terdekat

--- Exercise13.js
+++ Exercise13.js
@@ -1,27 +1,23 @@
 function targetTerdekat(arr) {
-    var shortest = 0;
-    var shortest2 = 0;
-    var latest = " ";
-      for(var x=0; x<arr.length; x++){
-          if(latest !== " " && latest !== arr[x] && " " !== arr[x]){
-              if(shortest2>shortest){
-                  shortest = shortest2;
-            }
-            shortest2 = 0;
-            latest = arr[x];
-        }
-          else if(latest !== " " && latest === arr[x]){
-              shortest2 = 0;
-        }
-          else if(latest !== " " && arr[x] === " "){
-              shortest2 += 1;
-          }
-          else if(arr[x] !== " "){
-              shortest2 += 1;
-              latest = arr[x];
-          }
+  var jarak=0;
+    for (var i=0; i<arr.length; i++){
+      if(arr[i]==="o"){
+        var countO=i;
       }
-    return(shortest);
+      else if(arr[i]==="x"){
+        var countX=i;
+      }
+
+      if (countX>countO){
+        jarak=countX-countO;
+        break;
+      }
+      else if (countX<countO){
+        jarak=countO-countX;
+        break;
+      }
+    }
+    return jarak;
   }
     
   // TEST CASES
@@ -30,3 +26,43 @@ function targetTerdekat(arr) {
   console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
   console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
   console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
+
+
+
+
+
+
+
+
+// function targetTerdekat(arr) {
+//     var shortest = 0;
+//     var shortest2 = 0;
+//     var latest = " ";
+//         for(var x=0; x<arr.length; x++){
+//             if(latest !== " " && latest !== arr[x] && " " !== arr[x]){
+//                 if(shortest2>shortest){
+//                     shortest = shortest2;
+//                 }
+//                 shortest2 = 0;
+//                 latest = arr[x];
+//             }
+//             else if(latest !== " " && latest === arr[x]){
+//                 shortest2 = 0;
+//             }
+//             else if(latest !== " " && arr[x] === " "){
+//                 shortest2 += 1;
+//             }
+//             else if(arr[x] !== " "){
+//                 shortest2 += 1;
+//                 latest = arr[x];
+//             }
+//         }
+//     return(shortest);
+//   }
+    
+//   // TEST CASES
+//   console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+//   console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+//   console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+//   console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+//   console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
